18cebc55df2c831210f60b623da72a936b65afcc
Sandi Krivec <sandi.krivec@hotmail.com>
2015-09-18 18:51:23 +0200

    2.9 Configuration file  // sandi

--- webserver/src/utilityHTTP.c
+++ webserver/src/utilityHTTP.c
@@ -10,7 +10,7 @@ void generateHeader(HTTPResponse *httpRes){
 	{
 		getFileCreationTime(httpRes->lastModified, SIZEOFDATETIMEGMT, httpRes->filePath); //dateTime.c
 	}
-	sprintf(httpRes->buffer,"HTTP/1.1 %s\nDate: %s\nLast-Modified: %s\nAllow: GET, HEAD\nServer: %s\nContent-Length: %ld\nContent-Type: %s\n\n",httpRes->statusCode, dateTimeGMT,httpRes->lastModified,SERVERNAME, httpRes->contentLength, httpRes->fileType);
+	sprintf(httpRes->buffer,"HTTP/1.0 %s\nDate: %s\nLast-Modified: %s\nAllow: GET, HEAD\nServer: %s\nContent-Length: %ld\nContent-Type: %s\n\n",httpRes->statusCode, dateTimeGMT,httpRes->lastModified,SERVERNAME, httpRes->contentLength, httpRes->fileType);
 }
 
 void UtilityFree(HTTPResponse *httpRes){
